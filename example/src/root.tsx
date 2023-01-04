fda1666bccdb3bfcff4bd482a377330b31e32956
xbmlz <997909544@qq.com>
2023-01-04 16:19:02 +0800

    feat: add styles

--- example/src/root.tsx
+++ example/src/root.tsx
@@ -21,19 +21,17 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Solid Markdown Demo</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body>
+      <Body class="font-sans bg-zinc-900 text-zinc-200">
         <Suspense>
           <ErrorBoundary>
-            <div id="app" class="font-sans bg-zinc-900 text-zinc-200">
-              <A href="/">Index</A> | <A href="/about">About</A>
-              <Routes>
-                <FileRoutes />
-              </Routes>
-            </div>
+            <A href="/">Index</A> | <A href="/about">About</A>
+            <Routes>
+              <FileRoutes />
+            </Routes>
           </ErrorBoundary>
         </Suspense>
         <Scripts />
